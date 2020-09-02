84707341c76baa1ff9a9e745ba04f4ecf177ceef
xs <xs16@rice.edu>
2020-09-02 13:25:51 -0500

    test utils

--- src/test_utils.h
+++ src/test_utils.h
@@ -3,13 +3,20 @@
 // create_one_pool_pp()
 // destroy_one_pool_pp()
 #include <rados/librados.hpp>
+#include<iostream> 
+#include<string>
+#include <sstream>
+
+using namespace librados;
 
 std::string create_one_pool_pp(const std::string &pool_name,
 			    librados::Rados &cluster);
 std::string create_one_pool_pp(const std::string &pool_name,
 			       librados::Rados &cluster,
 			       const std::map<std::string, std::string> &config);
-
+std::string create_one_ec_pool_pp(const std::string &pool_name,
+			    librados::Rados &cluster);
+std::string connect_cluster_pp(librados::Rados &cluster);
+std::string connect_cluster_pp(librados::Rados &cluster,
+			       const std::map<std::string, std::string> &config);
 int destroy_one_pool_pp(const std::string &pool_name, librados::Rados &cluster);
-
-std::string get_temp_pool_name(const std::string &prefix = "test-rados-api-");
