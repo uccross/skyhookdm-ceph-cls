a0941cd5c8e568631e5ae7b2e8dbfa6bdc00d574
Jeff LeFevre <jlefevre@soe.ucsc.edu>
2020-09-11 16:31:30 -0700

    update comment.

--- src/test_utils.h
+++ src/test_utils.h
@@ -4,7 +4,10 @@
 #include <string>
 
 using namespace librados;
+
+// the below functions are taken from ceph src tree:
 // https://github.com/ceph/ceph/blob/master/src/test/librados/test_shared.h
+
 std::string get_temp_pool_name(const std::string &prefix = "test-rados-api-");
 std::string create_one_pool_pp(const std::string &pool_name,
                                librados::Rados &cluster);
