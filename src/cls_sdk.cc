7f33960f6a98a2da43ec46d1c5a3fff68bd267fa
Jeff LeFevre <jlefevre@soe.ucsc.edu>
2020-09-11 16:38:16 -0700

    update class name.

--- src/cls_sdk.cc
+++ src/cls_sdk.cc
@@ -5,7 +5,7 @@
 #include "rados/objclass.h"
 
 CLS_VER(1, 0)
-CLS_NAME(sdk)
+CLS_NAME(cls_sdk)
 
 cls_handle_t h_class;
 cls_method_handle_t h_test_coverage_write;
@@ -119,9 +119,9 @@ static int test_coverage_replay(cls_method_context_t hctx,
 }
 
 CLS_INIT(cls_sdk) {
-  CLS_LOG(20, "loading sdk class");
+  CLS_LOG(20, "loading cls_sdk class");
 
-  cls_register("sdk", &h_class);
+  cls_register("cls_sdk", &h_class);
 
   cls_register_cxx_method(h_class, "test_coverage_write",
                           CLS_METHOD_RD | CLS_METHOD_WR, test_coverage_write,
